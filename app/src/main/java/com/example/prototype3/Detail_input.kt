3f01885ebde26461b7e5dc457d59dda4ec7dd2d1
k <zansanckzz@gmail.com>
2019-05-23 23:11:22 +0700

    Upload All Data Complete

--- app/src/main/java/com/example/prototype3/Detail_input.kt
+++ app/src/main/java/com/example/prototype3/Detail_input.kt
@@ -1,13 +1,22 @@
 package com.example.prototype3
 
+import android.app.Activity
+import android.content.ContentResolver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_girl_input.*
+import org.jetbrains.anko.startActivityForResult
+import java.util.*
 
 
 class Detail_input : AppCompatActivity() {
@@ -31,13 +40,21 @@ class Detail_input : AppCompatActivity() {
         nnamecare = findViewById(R.id.namecare_patient)
         ttelcare = findViewById(R.id.telcare_patient)
 
-
+        img_p_upload.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+        }
         spangirl_btn.setOnClickListener {
-            checkblank()
-          //  sentDb()
+           checkblank()
+
+          //sentDb()
+
 
         }
+
     }
+
     private fun checkblank(){
         val nnamepatientt = nnamepatient.text.toString()
         val llastnamepatientt = llastnamepatient.text.toString()
@@ -54,12 +71,12 @@ class Detail_input : AppCompatActivity() {
             return
         }else{
             val intent = Intent(this, span_symptom::class.java)
-            sentDb()
+            imageUpload()
             startActivity(intent)
         }
 
     }
-    private fun sentDb(){
+    private fun sentDb(imageuurl: String){
         val nnnamepatient = nnamepatient.text.toString().trim()
         val lllastnamepatient = llastnamepatient.text.toString().trim()
         val bbbd = bbd.text.toString().trim()
@@ -73,12 +90,56 @@ class Detail_input : AppCompatActivity() {
         val ref = FirebaseDatabase.getInstance().getReference("/Patient/$userId")
 
         val patientId = ref.push().key
-        val patient = sentToDbpatient(patientId.toString(),nnnamepatient,lllastnamepatient,bbbd,aaaadress,aaaadresscare,nnnamecare,tttelcare)
+        val patient = sentToDbpatient(patientId.toString(),nnnamepatient,lllastnamepatient,bbbd,aaaadress,aaaadresscare,nnnamecare,tttelcare,imageuurl)
 
         ref.child(patientId.toString()).setValue(patient).addOnCompleteListener {
+
+            Log.d("Detail_input","Upload All DATA SS")
             Toast.makeText(this,"บันทึกสำเร็จ", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
+    var selectedPhotoUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            Log.d("Detail_input","Photo Select")
+
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            img_p_upload.setBackgroundDrawable(bitmapDrawable)
+
+
         }
     }
+    private fun imageUpload() {
+        if (selectedPhotoUri == null){
+            Log.d("Detail_input","I AM HERE")
+            return
+        }
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d("Detail_input","Upload SS : ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d("Detail_input","Location: $it")
+                    sentDb(it.toString())
+                }
+
+            }
+            .addOnFailureListener{
+
+            }
+
+    }
+
+
 }
 
 
